48f8b2b068acd97ecf9ab4cb93a05dbcd169cd57
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2024-07-26 02:07:29 +0300

    feat: adds new route in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./layout/Layout";
 import Home from "./pages/Home";
+import Product from "./pages/Product";
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Layout />}>
           <Route index element={<Home />} />
+          <Route path='product/:id' element={<Product />} />
         </Route>
       </Routes>
     </BrowserRouter>
