03289e938daa2d77c7c0bc14193cad50bc76cfb9
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2024-07-26 02:06:58 +0300

    feat: adds new styles and Link in component Product

--- src/components/pages/home/products/Products.tsx
+++ src/components/pages/home/products/Products.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-
+import { Link } from "react-router-dom";
 interface Item {
   albumId: number;
   id: number;
@@ -29,7 +29,7 @@ const Products = () => {
   }, []);
 
   return (
-    <div className='mt-20 rounded-2xl border border-[#DDF6F2] px-8 py-6'>
+    <div className='rounded-2xl border border-[#DDF6F2] px-8 py-6'>
       <h2 className='border-b border-[#DDF6F2] pb-2 text-[#DDF6F2]'>Latest Drops</h2>
       <ul className='mt-5 flex flex-wrap items-center justify-center gap-4'>
         {(loading && <span className='text-9xl'>loading...</span>) ||
@@ -37,8 +37,10 @@ const Products = () => {
             data.map((item: Item) => {
               return (
                 <li key={item.id} className='max-w-40 rounded-2xl border border-[#DDF6F2] p-2'>
-                  <img src={item.thumbnailUrl} alt={item.title} />
-                  <p className='line-clamp-1'>{item.title}</p>
+                  <Link to={`product/${item.id}`}>
+                    <img src={item.thumbnailUrl} alt={item.title} />
+                    <p className='line-clamp-1 text-[#DDF6F2]'>{item.title}</p>
+                  </Link>
                 </li>
               );
             }))}
