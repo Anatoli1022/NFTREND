bf2b259f86f8544adb15bda7abd1a04e3e78cd05
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2024-08-05 17:23:18 +0300

    feat: adds new screens in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ export default {
   content: ["./node_modules/flowbite-react/lib/**/*.js", "./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    screens: {
+      xl: { max: "1279px" },
+
+      lg: { max: "1023px" },
+
+      md: { max: "767px" },
+
+      sm: { max: "639px" },
+    },
   },
   plugins: ["flowbite/plugin"],
 };
